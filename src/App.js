bc0cc7bea1f50da8a6f56582ec5b7eeaf39e524b
Salawi45 <benhmida.oussama@gmail.com>
2024-11-07 23:28:39 +0100

    Success

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="div-centre">
+        <h2 className="my-text">Je serai installable!</h2>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
